6a048a63e66d031ab83bf9fbf4f670b4dcee7260
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-08-28 11:11:03 -0300

    Comentarios por seção

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
+/*
+Autor: Denilson Gomes Vaz da Silva
+Graduando em Engenharia da Computação - UFC
+
+Código para leitura de presença usando sensor PIR
+*/
+
+//--Bibliotecas
 #include <Arduino.h>
 
+//--Variaveis e COnstantes
 #define pinoPIR 2
 #define pinoSaida 13
 int valor = 0;
 
+//--Setup
 void setup() {
     pinMode(pinoPIR, INPUT);
     pinMode(pinoSaida, OUTPUT);
     Serial.begin(9600);
 }
 
+//Loop
 void loop() {
     valor = digitalRead(pinoPIR);
     while(valor){
